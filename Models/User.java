5edba4a1404e8ea1935a3658ebcfac858696ffec
KevinPhilips-hue <kevin.k.philips@gmail.com>
2025-11-02 19:28:55 +0800

    Added basic definition to filterBy method in User.java

--- Models/User.java
+++ Models/User.java
@@ -59,8 +59,62 @@ public class User {
 
     }
 
-    void filterBy(String filter) {
+    void filterBy() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number for the filter you want to apply:\n"
+                   + "1: Status\n"
+                   + "2: Preferred Majors\n"
+                   + "3: Internship Level\n"
+                   + "4: Closing Date");
+        
+        int filter = scanner.nextInt();
+        switch (filter) {
+            case 1: // Do in while loop
+                System.out.println("Enter the status of the internship:\n"
+                   + "1: Pending\n"
+                   + "2: Approved\n"
+                   + "3: Rejected\n"
+                   + "4: Filled");
+                String status = scanner.next();
+                // Do not need switch case, can just loop through internships and print those == status
+                switch (status) {
+                    case "Pending":
+                        // ...
+                        break;
+                    case "Approved":
+                        // ...
+                        break;
+                    case "Rejected":
+                        // ...
+                        break;
+                    case "Filled":
+                        // ...
+                        break;
+                    default:
+                        System.out.println("Invalid status! Please try again"); // Prompt again
+                }
+                break;
+            case 2:
+                System.out.println("Please input your preferred major");
+                String major = scanner.nextLine();
+                // ...
+                break;
+            case 3:
+                System.out.println("Please input your preferred internship level");
+                String level = scanner.nextLine();
+                // ...
+                break;
+            case 4:
+                System.out.println("Please input your closing date");
+                // Use date object
+                // ...
+                break;
+            default:
+                System.out.println("Invalid filter!"); // Prompt again
+        }
 
+        scanner.close();
+            
     }
 }
 
